df9b2da5778821fc4d5a5b8e80c38b308089bd6e
Jaime Longoria <jaime.longoria@journalism.cuny.edu>
2019-12-10 01:04:30 -0500

    overall improvements on graphic

--- main.js
+++ main.js
@@ -10,24 +10,13 @@ $(document).ready(() => {
 			}, 10 * (1 + (i / 10)));
 
 		});
-
 		$(".arrow").addClass("arrow_hidden");
-
+		$(".no_conviction.dot_gray").removeClass("dot_gray");
 	});
 
-
-
-
-
-
-
-
-
-	//	watcher.outView(() => {
-	//		$(".dot.dot_appear").removeClass("dot_appear");
-	//	});
-
-	console.log(watcher);
+	watcher.outView(() => {
+		$(".arrow.arrow_hidden").removeClass("arrow_hidden");
+	});
 });
 
 
@@ -39,22 +28,27 @@ $(document).ready(() => {
 
 	watcher.inView(() => {
 		$(".no_conviction").addClass("dot_gray");
+		$(".graphic.metrics").removeClass("metrics");
 	});
 });
+
 
 $(document).ready(() => {
 	const watcher = new Watch($("#zoom"));
 
 	watcher.inView(() => {
 		$(".graphic").addClass("metrics");
+		$(".sell.dot_sell").removeClass("dot_sell");
 	});
 });
+
 
 $(document).ready(() => {
 	const watcher = new Watch($("#sell"));
 
 	watcher.inView(() => {
 		$(".sell").addClass("dot_sell");
+		$(".possession.dot_possession").removeClass("dot_possession");
 	});
 });
 
@@ -64,6 +58,7 @@ $(document).ready(() => {
 
 	watcher.inView(() => {
 		$(".possession").addClass("dot_possession");
+		$(".marijuana_default.dot_default").removeClass("dot_default");
 	});
 });
 
@@ -73,6 +68,7 @@ $(document).ready(() => {
 
 	watcher.inView(() => {
 		$(".marijuana_default").addClass("dot_default");
+		$(".smuggle.dot_smuggle").removeClass("dot_smuggle");
 	});
 });
 
@@ -82,5 +78,15 @@ $(document).ready(() => {
 
 	watcher.inView(() => {
 		$(".smuggle").addClass("dot_smuggle");
+		$(".conclude.dot_conclude").removeClass("dot_conclude");
+	});
+});
+
+
+$(document).ready(() => {
+	const watcher = new Watch($("#conclude"));
+
+	watcher.inView(() => {
+		$(".conclude").addClass("dot_conclude");
 	});
 });
